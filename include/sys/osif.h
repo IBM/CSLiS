56a464d111ee9639c642b8bb3dfa4de0e4906012
Jeff L Smith <111383774+jefsmithIBM@users.noreply.github.com>
2026-01-09 15:22:31 -0500

    Update osif.h for 7.1.1.3
    Signed-off-by: Jeff L Smith jefsmith@us.ibm.com

--- include/sys/osif.h
+++ include/sys/osif.h
@@ -45,7 +45,8 @@
 #endif
 #endif
 #if (defined(_S390X_LIS_) || defined(_PPC64_LIS_) )
-#if (defined(RHEL_RELEASE_CODE) && RHEL_RELEASE_CODE > 2309)
+#if ((defined(RHEL_RELEASE_CODE) && RHEL_RELEASE_CODE > 2309) || \
+     (LINUX_VERSION_CODE > KERNEL_VERSION(6,10,0))) /* RHEL 9.6 or RHEL 10, SLES 16 */		
 #define _LINUX_PROPERTY_H_  // omit property.h
 #endif		
 #endif
